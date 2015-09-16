c6389f0862b5f3ed3a3d59b5e27c621dbeaffe3c
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-09-16 22:18:01 +0200

    Added gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var config = require('./gulp.config')();
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+
+var plugins = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('lint', function() {
   return gulp.src(config.alljs)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
