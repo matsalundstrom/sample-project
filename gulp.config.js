e6c97510c403af394cbf9611eaf8c3b4dc739e83
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-09-16 22:05:02 +0200

    Added lint

---
+++ gulp.config.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+
+    var config = {
+        alljs: [
+            './src/**/*js',
+            './*.js'
+        ],
+    };
+
+    return config;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var config = require('./gulp.config')();
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function () {
-
+gulp.task('lint', function() {
+  return gulp.src(config.alljs)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
